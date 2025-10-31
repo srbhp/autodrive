df8a2e503f14397ee5928c89a9c362ceb64b19a7
Saurabh Pradhan <pradhanphy@gmail.com>
2025-10-31 11:12:26 +0100

    fastpi update

---
+++ README.md
@@ -0,0 +1,11 @@
+# autodrive
+
+## detect traffic sign
+
+- `uvicorn main:app --reload`
+- open the `index.html` file in the firefox/chrome.
+
+## resources
+
+- https://github.com/lyft/nuscenes-devkit
+- Nvidia Data: https://github.com/NVlabs/physical_ai_av 
--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from fastapi import FastAPI
-import cv2
-from fastapi import UploadFile
+from fastapi import FastAPI, UploadFile, File
 import numpy as np
+import cv2
 from ultralytics import YOLO
+import io
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow CORS for frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# Load the YOLO model
 model = YOLO("saved_model/trafic_sign_model.pt")
 
 
 @app.post("/detect/")
-async def detect_objects(file: UploadFile):
-    # Process the uploaded image for object detection
+async def detect_objects(file: UploadFile = File(...)):
+    # Read the uploaded file
     image_bytes = await file.read()
-    image = np.frombuffer(image_bytes, dtype=np.uint8)
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-    # Perform object detection with YOLOv8
-    detections = model.predict(image)
+    # Convert bytes to NumPy array
+    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
 
-    return {"detections": detections}
+    # Decode the image
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    # Perform object detection
+    results = model.predict(image, save=True)
+    for result in results:
+        result_image = result.plot()
+
+    # Encode image to JPEG
+    _, buffer = cv2.imencode(".jpg", result_image)
+    image_bytes = io.BytesIO(buffer)
+
+    return StreamingResponse(image_bytes, media_type="image/jpeg")
